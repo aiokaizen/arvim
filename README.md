c38898623c079aed4f8eecea5c1b610c4c16249a
Mouad Kommir <mouadkommir@gmail.com>
2024-11-15 15:03:30 +0100

    First commit - Basic options

---
+++ README.md
@@ -0,0 +1,12 @@
+# ARVIM
+
+## Description
+
+This is an nvim configuration created from scratch and that is optimized for python development.
+
+
+## Options
+
+
+## Keymaps
+
---
+++ init.lua
@@ -0,0 +1,21 @@
+-- Line numbers
+vim.opt.number = true
+vim.opt.rnu = true
+
+-- Window Splits
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- Line wrap
+vim.opt.wrap = false
+
+-- Tab behaviour
+vim.opt.expandtab = true  -- Expands tabs into spaces
+vim.opt.tabstop = 4
+
+-- Clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- Scroll behaviour
+vim.opt.scrolloff = 999  -- Scroll stopps in the middle instead of gowing all the way down
+
