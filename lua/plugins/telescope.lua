615ac6a839acd16a31e67e92891d94c8d3ea7d00
Mouad Kommir <mouadkommir@gmail.com>
2024-11-19 15:20:31 +0100

    Update telescope configuration

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 local themes = {
     "rebelot/kanagawa.nvim",
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 }
 
 local telescope = {
@@ -11,21 +11,26 @@ local telescope = {
         'andrew-george/telescope-themes',
         themes,
     },
-    opts = {
-        defaults = {
-            file_ignore_patterns = { "node_modules", "venv" },
-        },
-    },
     config = function()
         local builtin = require('telescope.builtin')
         local telescope = require('telescope')
 
         -- Set keymaps after telescope is loaded
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+        vim.keymap.set('n', '<leader>ff', function()
+            builtin.find_files({ hidden = false, no_ignore = true, no_ignore_parent = true })
+        end, { desc = 'Telescope find files (Include .gitignored files)' })
+        vim.keymap.set('n', '<leader>fg', function()
+            builtin.live_grep({ hidden = false, no_ignore = true, no_ignore_parent = true })
+        end, { desc = 'Telescope live grep (Include .gitignored files)' })
         vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
         vim.keymap.set('n', '<leader>th', "<cmd> Telescope themes <CR>", { desc = 'Telescope themes' })
+
+        telescope.setup({
+            defaults = {
+                file_ignore_patterns = { "node_modules", "venv", "__pycache__" },
+            },
+        })
 
         -- Load extensions
         telescope.load_extension('themes')
