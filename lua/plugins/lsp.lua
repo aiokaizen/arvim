cad5835c7ac7f3f6f58af48b38de1b7ac3bdd3a5
Mouad Kommir <mouadkommir@gmail.com>
2024-11-19 20:48:24 +0100

    Add autocompletion capabilities using nvim-cmp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,73 +1,113 @@
 return {
-    {
-        "williamboman/mason-lspconfig.nvim",
-        dependencies = { "williamboman/mason.nvim", },
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls",
-                    "rust_analyzer",
-                    "pyright",
-                },
-                handlers = {
-                    -- Default handler
-                    function(server_name)
-                        require("lspconfig")[server_name].setup {}
-                    end,
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+		},
+		config = function()
+			local cmp = require("cmp")
 
-                    -- Dedicated handlers for specific servers.
-                    ["pyright"] = function()
-                        local lspconfig = require("lspconfig")
-                        lspconfig.pyright.setup({
-                            settings = {
-                                pyright = {
-                                    -- Using Ruff's import organizer
-                                    -- disableOrganizeImports = true,
-                                },
-                                python = {
-                                    analysis = {
-                                        -- Ignore all files for analysis to exclusively use Ruff for linting
-                                        -- ignore = { '*' },
-                                    }
-                                }
-                            }
-                        })
-                    end,
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item.
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "buffer" },
+					{ name = "path" },
+				}),
+			})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"rust_analyzer",
+					"pyright",
+				},
+				handlers = {
+					-- Default handler
+					function(server_name)
+						require("lspconfig")[server_name].setup({})
+					end,
 
-                    ["ruff"] = function()
-                        local lspconfig = require("lspconfig")
-                        lspconfig.ruff.setup({
-                            trace = 'messages',
-                            init_options = {
-                                settings = {
-                                    configuration = "~/.config/nvim/ruff.toml",
-                                    logLevel = 'debug',
-                                    -- exclude = ["**/venv/**"],
-                                },
-                            },
-                        })
-                    end,
+					-- Dedicated handlers for specific servers.
+					["pyright"] = function()
+						local lspconfig = require("lspconfig")
 
-                    ["lua_ls"] = function()
-                        local lspconfig = require("lspconfig")
-                        lspconfig.lua_ls.setup {
-                            settings = {
-                                Lua = {
-                                    diagnostics = {
-                                        globals = { "vim" }
-                                    }
-                                }
-                            }
-                        }
-                    end,
-                }
+						lspconfig.pyright.setup({
+							settings = {
+								-- pyright = { },
+								-- python = { analysis = { }, },
+							},
+							on_attach = function(client, bufnr)
+								-- Enable completion triggered by <C-x><C-o>
+								vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-            })
-        end
-    },
-    {
-        -- LSP: Language Server Protocol
-        "neovim/nvim-lspconfig",
-        dependencies = { "williamboman/mason-lspconfig.nvim", },
-    }
+								-- Key mappings
+								local opts = { noremap = true, silent = true }
+								vim.api.nvim_buf_set_keymap(
+									bufnr,
+									"n",
+									"gd",
+									"<Cmd>lua vim.lsp.buf.definition()<CR>",
+									opts
+								)
+								vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+							end,
+							capabilities = require("cmp_nvim_lsp").default_capabilities(),
+						})
+					end,
+
+					["ruff"] = function()
+						local lspconfig = require("lspconfig")
+						lspconfig.ruff.setup({
+							trace = "messages",
+							init_options = {
+								settings = {
+									configuration = "~/.config/nvim/ruff.toml",
+									logLevel = "debug",
+									-- exclude = ["**/venv/**"],
+								},
+							},
+						})
+					end,
+
+					["lua_ls"] = function()
+						local lspconfig = require("lspconfig")
+						lspconfig.lua_ls.setup({
+							settings = {
+								Lua = {
+									diagnostics = {
+										globals = { "vim" },
+									},
+								},
+							},
+						})
+					end,
+				},
+			})
+		end,
+	},
+	{
+		-- LSP: Language Server Protocol
+		"neovim/nvim-lspconfig",
+		dependencies = { "williamboman/mason-lspconfig.nvim" },
+	},
 }
